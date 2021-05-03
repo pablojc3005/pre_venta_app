d55b763cfbc43bfcb6de67fc2daf6271b775a5a0
pablojc3005 <58855957+pablojc3005@users.noreply.github.com>
2021-05-03 17:40:06 -0500

    avance dia 29-04-2021

---
+++ app/src/main/java/com/example/pre_venta_app/Datos/DSucursal.java
@@ -0,0 +1,96 @@
+package com.example.pre_venta_app.Datos;
+
+import android.util.Log;
+
+import com.example.pre_venta_app.Entidad.Sucursal;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Types;
+import java.util.ArrayList;
+
+public class DSucursal {
+
+    public static ArrayList<Sucursal> Lista_sucursales() {
+        ArrayList<Sucursal> lista = new ArrayList<>();
+        Connection cn = Conexion.Conectar();
+        String sql = " select s.CodCliente, C.DesCliente, s.CodSucursal, s.DesSucursal, s.DirentregaSuc from SucursalCliente s \n" +
+                     " inner join Clientes c on S.CodCliente = C.CodCliente \n" +
+                     " where s.status_registro = 'V';";
+        try {
+            PreparedStatement pst = cn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next())
+            {
+                lista.add(new Sucursal(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
+            }
+            Log.e("lista sucursales : ", lista.toString());
+        }
+        catch (java.sql.SQLException e)
+        {
+            Log.e("Excepcion 01", e.toString());
+        }
+        catch (Exception e) {
+            Log.e("Excepcion 02", e.toString());
+        }
+        return  lista;
+    }
+
+    public static String Insertar_sucursal(Sucursal c) {
+        String rpta = null;
+        Connection cn = null;
+        CallableStatement pst = null;
+        ResultSet rs = null;
+        try {
+            cn = Conexion.Conectar();
+            pst = cn.prepareCall("{call usp_sucursal_insert(?,?,?,?,?)}");
+            pst.setString(1, c.getCod_cliente());
+            Log.e("getCod_cliente ", c.getCod_cliente());
+            pst.setString(2, c.getCod_sucursal());
+            Log.e("getCod_sucursal ", c.getCod_sucursal());
+            pst.setString(3, c.getDes_sucursal());
+            Log.e("getDes_sucursal ", c.getDes_sucursal());
+            pst.setString(4, c.getDireccion());
+            Log.e("getDireccion ", c.getDireccion());
+            pst.registerOutParameter(6, Types.VARCHAR);
+            pst.execute();
+            rpta = pst.getString(6);
+            Log.e("rpta ", rpta);
+            cn.close();
+
+        } catch (Exception ex) {
+            Log.e("Error", ex.getMessage());
+        }
+        return rpta;
+    }
+
+    public static String Actualizar_cliente(Sucursal c) {
+        String rpta = null;
+        Connection cn = null;
+        CallableStatement pst = null;
+        ResultSet rs = null;
+        try {
+            cn = Conexion.Conectar();
+            pst = cn.prepareCall("{call usp_sucursal_update(?,?,?,?,?)}");
+            pst.setString(1, c.getCod_cliente());
+            Log.e("getCod_cliente ", c.getCod_cliente());
+            pst.setString(2, c.getCod_sucursal());
+            Log.e("getCod_sucursal ", c.getCod_sucursal());
+            pst.setString(3, c.getDes_sucursal());
+            Log.e("getDes_sucursal ", c.getDes_sucursal());
+            pst.setString(4, c.getDireccion());
+            Log.e("getDireccion ", c.getDireccion());
+            pst.registerOutParameter(6, Types.VARCHAR);
+            pst.execute();
+            rpta = pst.getString(6);
+            Log.e("rpta ", rpta);
+            cn.close();
+
+        } catch (Exception ex) {
+            Log.e("Error", ex.getMessage());
+        }
+        return rpta;
+    }
+}
--- app/src/main/java/com/example/pre_venta_app/Entidad/Sucursal.java
+++ app/src/main/java/com/example/pre_venta_app/Entidad/Sucursal.java
@@ -2,4 +2,60 @@ package com.example.pre_venta_app.Entidad;
 
 public class Sucursal {
 
+    String cod_cliente;
+    String desc_cliente;
+    String cod_sucursal;
+    String des_sucursal;
+    String direccion;
+
+    public String getCod_cliente() {
+        return cod_cliente;
+    }
+
+    public void setCod_cliente(String cod_cliente) {
+        this.cod_cliente = cod_cliente;
+    }
+
+    public String getDesc_cliente() {
+        return desc_cliente;
+    }
+
+    public void setDesc_cliente(String desc_cliente) {
+        this.desc_cliente = desc_cliente;
+    }
+
+    public String getCod_sucursal() {
+        return cod_sucursal;
+    }
+
+    public void setCod_sucursal(String cod_sucursal) {
+        this.cod_sucursal = cod_sucursal;
+    }
+
+    public String getDes_sucursal() {
+        return des_sucursal;
+    }
+
+    public void setDes_sucursal(String des_sucursal) {
+        this.des_sucursal = des_sucursal;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Sucursal() {
+    }
+
+    public Sucursal(String cod_cliente, String desc_cliente, String cod_sucursal, String des_sucursal, String direccion) {
+        this.cod_cliente = cod_cliente;
+        this.desc_cliente = desc_cliente;
+        this.cod_sucursal = cod_sucursal;
+        this.des_sucursal = des_sucursal;
+        this.direccion = direccion;
+    }
 }
